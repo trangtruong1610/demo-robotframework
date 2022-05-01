f286efab5b558a31e406e8bf3ab7c82185532273
trang.truong@gigacover.com <trang.truong@gigacover.com>
2022-05-01 15:41:10 +0700

    add more webdriver

--- webdriver/selenium_webdriver.py
+++ webdriver/selenium_webdriver.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.service import Service
 filepath = os.path.dirname(__file__)
 IMPLICITLY_WAIT = 10
 
-def wd_chrome():
+def wd_chrome_local():
     option = webdriver.ChromeOptions()
     # option.headless = True
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
@@ -17,6 +17,13 @@ def wd_firefox():
     option = webdriver.FirefoxOptions()
     option.headless = True
     driver = webdriver.Firefox(options=option)
+    driver.implicitly_wait(IMPLICITLY_WAIT)
+    return driver
+
+def wd_chrome():
+    option = webdriver.ChromeOptions()
+    option.headless = True
+    driver = webdriver.Chrome(options=option)
     driver.implicitly_wait(IMPLICITLY_WAIT)
     return driver
 
