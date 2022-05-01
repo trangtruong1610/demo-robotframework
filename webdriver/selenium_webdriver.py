50b22334d8810cff8f121c612f61d3a5a1d15553
trang.truong@gigacover.com <trang.truong@gigacover.com>
2022-05-01 13:40:49 +0700

    demo run pytest with jenkins

--- webdriver/selenium_webdriver.py
+++ webdriver/selenium_webdriver.py
@@ -1,13 +1,15 @@
 import os
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 filepath = os.path.dirname(__file__)
 IMPLICITLY_WAIT = 10
 
 def wd_chrome():
     option = webdriver.ChromeOptions()
-    option.headless = True
-    driver = webdriver.Chrome(options=option)
+    # option.headless = True
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
     driver.implicitly_wait(IMPLICITLY_WAIT)
     return driver
 
